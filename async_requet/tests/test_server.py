8811c76f62c996b3766a989eec975dddc31fbe63
mohamad khajezade <khajezade.mohamad@gmail.com>
2019-09-15 15:13:20 +0430

    Test server

--- async_requet/tests/test_server.py
+++ async_requet/tests/test_server.py
@@ -1,5 +1,4 @@
 import time
-import unittest
 import requests
 
 
@@ -7,5 +6,6 @@ class TestMockServer:
 
     def test_mockserver(self, mock_server):
         time.sleep(1)
-        r = requests.get('http://localhost:5000/todo/api/v1.0/tasks/2')
-        assert 1 ==1
+        response = requests.get('http://localhost:5000/todo/api/v1.0/tasks/2')
+        assert response.json()['id'] == 2
+
